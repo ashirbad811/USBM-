93a2711cac548dd2fd2e80323fe93976ee94c2b3
ASHIRBAD MISHRA <82135004+ashirbad811@users.noreply.github.com>
2024-10-07 11:23:00 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+// console.log("hello world!");
+// let name = "Ashirbad";
+// var age = 22;
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+// var city = "bhubaneswar";
+
+// console.log(city);
+
+// let year = null ;
+// console.log(typeof null);
+
+//undefined
+// let age ;
+// console.log(typeof age);
+// console.log(age);
+
+//number 
+let num1 = 10 ;
+// console.log(typeof num1);
+
+// let float =12.5 ;
+// console.log(typeof float);
+
+// let  rollNo = 012 ;
+// console.log(rollNo);
+
+let num2 =8 ;
+let sum = num1+num2 ;
+let substraction = num1 - num2 ;
+let multi = num1 * num2 ;
+let division = num1/num2 ;
+console.log(sum);
+console.log(substraction);
+console.log(multi);
+console.log(division);
+
+
+
+
